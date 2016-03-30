6b8b3ac9741b96335b887b65d00a79560e0d099c
Ben Greenier <begreen@microsoft.com>
2016-03-30 16:42:39 -0400

    v0.1.0 - basic /giphy support

---
+++ index.js
@@ -0,0 +1,32 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var request = require('request');
+
+var server = restify.createServer();
+
+var giphyBot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
+giphyBot.add('/', function (session) {
+    var matched = /\/giphy\s(.+)/.exec(session.message.text);
+    if (matched && matched.length > 0) {
+        request("http://api.giphy.com/v1/gifs/search?q="+matched[1]+"&api_key=dc6zaTOxFJmzC&limit=1", function (err, res, body) {
+            if (err) {
+                session.error(err);
+            } else {
+                var url = JSON.parse(body).data[0].url;
+                var msg = session.createMessage(url);
+                
+                session.send(msg);
+            }
+        });
+    } else {
+        //TODO: uncomment if we stop listening on all input
+        //session.error(new Error("invalid input"));
+    }
+});
+
+server.use(giphyBot.verifyBotFramework());
+server.post('/v1/messages', giphyBot.listen());
+
+server.listen(process.env.PORT || 3000, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
